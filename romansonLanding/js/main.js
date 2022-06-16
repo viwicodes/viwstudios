712c4cec5810c291f4b24b0baaa0a2b08b548ed5
viwicodes <viwicodes@gmail.com>
2022-06-16 12:54:05 +0530

    check again increment error

--- romansonLanding/js/main.js
+++ romansonLanding/js/main.js
@@ -41,10 +41,10 @@
 
 
     // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
-    });
+    // $('[data-toggle="counter-up"]').counterUp({
+    //     delay: 10,
+    //     time: 2000
+    // });
 
 
     // Date and time picker
